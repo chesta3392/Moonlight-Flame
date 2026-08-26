d1232d7a22864fa2cbd71fd8269fd0b0afbc8032
oyeanmol <anmolshah1707@gmail.com>
2026-08-26 15:59:25 +0530

    fix: resolve favicon and anonymous cart behavior

--- candle/urls.py
+++ candle/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url='/media/favicon.ico', permanent=True)),
+    path('favicon-32x32.png', RedirectView.as_view(url='/media/favicon-32x32.png', permanent=True)),
+    path('apple-touch-icon.png', RedirectView.as_view(url='/media/apple-touch-icon.png', permanent=True)),
     path('', include('store.urls')),
     path('accounts/', include('accounts.urls')),
 ]
